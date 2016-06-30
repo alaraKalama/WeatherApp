3096c642779145ac8139216b5d9f13101615fd9a
Bianca Hinova <bianca.hinova@gmail.com>
2016-06-30 16:20:14 +0300

    basic storyboard things

---
+++ WeatherApp/PlaceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlaceTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Bianca Hinova on 6/30/16.
+//  Copyright © 2016 Bianca Hinova. All rights reserved.
+//
+
+import UIKit
+
+class PlaceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WeatherApp/PlacesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Bianca Hinova on 6/30/16.
+//  Copyright © 2016 Bianca Hinova. All rights reserved.
+//
+
+import UIKit
+
+class PlacesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
