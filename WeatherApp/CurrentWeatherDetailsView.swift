7211f23b624c6ebaa7fac8dd9880f3b601e89202
Bianca Hinova <bianca.hinova@gmail.com>
2016-07-07 18:12:51 +0300

    scroll view some work

---
+++ WeatherApp/CurrentWeatherDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentWeatherDetailsView.swift
+//  WeatherApp
+//
+//  Created by Bianca Hinova on 7/7/16.
+//  Copyright © 2016 Bianca Hinova. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherDetailsView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
