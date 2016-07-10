6d1434a44fe46eee6c90caa88b9a20e09886d168
Bianca Hinova <bianca.hinova@gmail.com>
2016-07-10 16:37:00 +0300

    details view controller background changes and parse all current weather info

--- WeatherApp/DetailsView.swift
+++ WeatherApp/DetailsView.swift
@@ -26,48 +26,82 @@ class DetailsView: UIView {
     @IBOutlet weak var windSpeedValueLabel: UILabel!
     
     func displayPlaceInfo(place: Place) {
-        downloadManager.downloadImage(Constants.githubUrl + Constants.cloudydayUrl, view: self.backgroundImageView)
+        
         self.summaryLabel.text = place.summary
         self.iconView.image = UIImage(named: place.icon!)
+        var backgroundURL = String()
+        switch place.icon {
+        case Constants.clear_day?:
+            backgroundURL = Constants.cleardayUrl
+        case Constants.clear_night?:
+            backgroundURL = Constants.clearnightUrl
+        case Constants.cloudy_day?:
+            backgroundURL = Constants.cloudydayUrl
+        case Constants.cloudy_night?:
+            backgroundURL = Constants.cloudynightUrl
+        case Constants.partly_cloudy_day?:
+            backgroundURL = Constants.partlycloudydayUrl
+        case Constants.partly_cloudy_night?:
+            backgroundURL = Constants.cloudynightUrl
+        case Constants.rain?:
+            backgroundURL = Constants.raindayUrl
+        case Constants.sleet?:
+            backgroundURL = Constants.sleetUrl
+        case Constants.snow?:
+            backgroundURL = Constants.snowdayUrl
+        case Constants.fog?:
+            backgroundURL = Constants.fogUrl
+        default:
+            backgroundURL = Constants.cleardayUrl
+        }
         
-//        if let humidity = place.humidity {
-//            self.humidityValueLabel.text = "\(humidity)%"
-//        }
-//
-//        if GlobalUnit.sharedManager.isCelsiusSystem {
-//            if let temp = place.currentTemperatureC {
-//                self.temperatureLabel.text = "\(temp)°C"
-//            }
-//            if let feelsLike = place.feelsLikeTemperatureC {
-//                self.feelsLikeValueLabel.text = "\(feelsLike)°C"
-//            }
-//            if let visibility = place.visibilityKm {
-//                //\(someDouble.format(.1))"
-//                self.visibilityValueLabel.text = String(format: "%.1f km", visibility)
-//            }
-//            if let pressure = place.pressureMb {
-//                self.pressureValueLabel.text = "\(Int(pressure)) mb"
-//            }
-//            if let windSpeed = place.windSpeedKm {
-//                self.windSpeedValueLabel.text = String(format: "%.1f km/h", windSpeed)
-//            }
-//        } else {
-//            if let temp = place.currentTemperatureF {
-//                self.temperatureLabel.text = "\(temp)°F"
-//            }
-//            if let feelsLike = place.feelsLikeTemperatureF {
-//                self.feelsLikeValueLabel.text = "\(feelsLike)°C"
-//            }
-//            if let visibility = place.visibilityMi {
-//                //\(someDouble.format(.1))"
-//                self.visibilityValueLabel.text = String(format: "%.1f mi", visibility)
-//            }
-//            if let pressure = place.pressureIn {
-//                self.pressureValueLabel.text = "\(Int(pressure)) in"
-//            }
-//            if let windSpeed = place.windSpeedMi {
-//                self.windSpeedValueLabel.text = String(format: "%.1f mi/h", windSpeed)
-//            }
-//        }
+        downloadManager.downloadImage(Constants.githubUrl + backgroundURL, view: self.backgroundImageView)
+        
+        if let humidity = place.humidity {
+            self.humidityValueLabel.text = "\(humidity)%"
+        }
+        
+        if let precipProbability = place.precipitation {
+            self.chanceOfValueLabel.text = "\(precipProbability) %"
+        }
+        if let precipType = place.precipType {
+            self.chanceOfDescriptionLabel.text = "Chance of \(precipType)"
+        } else {
+            self.chanceOfDescriptionLabel.text = "Chance of rain"
+        }
+
+        if GlobalUnit.sharedManager.isCelsiusSystem {
+            if let temp = place.currentTemperatureC {
+                self.temperatureLabel.text = "\(temp)°C"
+            }
+            if let feelsLike = place.feelsLikeTemperatureC {
+                self.feelsLikeValueLabel.text = "\(feelsLike)°C"
+            }
+            if let visibility = place.visibilityKm {
+                self.visibilityValueLabel.text = String(format: "%.1f km", visibility)
+            }
+            if let pressure = place.pressureMb {
+                self.pressureValueLabel.text = "\(Int(pressure)) mb"
+            }
+            if let windSpeed = place.windSpeedKm {
+                self.windSpeedValueLabel.text = String(format: "%.1f km/h", windSpeed)
+            }
+        } else {
+            if let temp = place.currentTemperatureF {
+                self.temperatureLabel.text = "\(temp)°F"
+            }
+            if let feelsLike = place.feelsLikeTemperatureF {
+                self.feelsLikeValueLabel.text = "\(feelsLike)°C"
+            }
+            if let visibility = place.visibilityMi {
+                self.visibilityValueLabel.text = String(format: "%.1f mi", visibility)
+            }
+            if let pressure = place.pressureIn {
+                self.pressureValueLabel.text = "\(Int(pressure)) in"
+            }
+            if let windSpeed = place.windSpeedMi {
+                self.windSpeedValueLabel.text = String(format: "%.1f mi/h", windSpeed)
+            }
+        }
     }
 }
